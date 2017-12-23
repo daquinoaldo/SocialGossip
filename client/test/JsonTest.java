e81825db1c9f2e7f62a54b9c7f2bf0bc20ccee59
Antonio Pitasi <pitasi.antonio@gmail.com>
2017-12-23 14:07:26 +0100

    stupidissimo test per la libreria dei json utile per vedere se l'ha importata correttamente
    Signed-off-by: Antonio Pitasi <pitasi.antonio@gmail.com>

---
+++ client/test/JsonTest.java
@@ -0,0 +1,14 @@
+import org.json.simple.JSONObject;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class JsonTest {
+    @Test
+    void SimpleJsonTest() {
+        JSONObject jsonObj = new JSONObject();
+        jsonObj.put("key", "value");
+        
+        assertEquals("{\"key\":\"value\"}", jsonObj.toJSONString(), "JSON creation and toJSONString");
+    }
+}
