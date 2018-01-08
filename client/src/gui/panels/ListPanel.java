8c558ad2e5cc998ef593d67c06868e0eb0f27ccc
Aldo D'Aquino <daquino.aldo@gmail.com>
2018-01-08 17:03:20 +0100

    ListPanel factory

--- client/src/gui/panels/ListPanel.java
+++ client/src/gui/panels/ListPanel.java
@@ -1,52 +1,39 @@
 package gui.panels;
 
-import base.State;
-import constants.Dimensions;
-import gui.Utils;
-
 import javax.swing.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
 
-import base.State.Friend;
+import constants.Dimensions;
+import gui.Utils;
 
-class ListPanel extends JPanel {
+public class ListPanel extends JPanel {
 
-    private static final String DEFAULT_LABEL = "Double-click to start chat with...";
-
-    private static String[] toArray(Collection<Friend> friends, boolean onlineOnly) {
-        List<String> online = new ArrayList<>();
-        List<String> offline = new ArrayList<>();
-        for (Friend friend : friends) {
-            if(friend.isOnline()) online.add(friend.getUsername());
-            else offline.add(friend.getUsername());
-        }
-        if (!onlineOnly) online.addAll(offline);
-        return online.toArray(new String[online.size()]);
-    }
-
-    ListPanel(Collection<Friend> friends, boolean onlineOnly) {
-        this(DEFAULT_LABEL, friends, onlineOnly);
-    }
-    ListPanel(String labelText, Collection<Friend> friends, boolean onlineOnly) {
-        this(labelText, toArray(friends, onlineOnly));
-    }
-    ListPanel(String labelText, String[] chats) {
-        JList<String> jlist = new JList<>(chats);
-        JScrollPane listScroller = new JScrollPane(jlist);
-        listScroller.setAlignmentX(LEFT_ALIGNMENT);
-
+    public ListPanel(String firstText, String[] firstGroup, String secondText, String[] secondGroup) {
         this.setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
-        JLabel label = new JLabel(labelText);
-        this.add(label);
-        this.add(Dimensions.V_SPACER);
-        this.add(listScroller);
         this.setBorder(Dimensions.PADDING_BORDER);
 
+        JList<String> firstList = new JList<>(firstGroup);
+        JList<String> secondList = new JList<>(secondGroup);
+
+        JScrollPane firstPanel = new JScrollPane(firstList);
+        firstPanel.setAlignmentX(LEFT_ALIGNMENT);
+        JScrollPane secondPanel = new JScrollPane(secondList);
+        secondPanel.setAlignmentX(LEFT_ALIGNMENT);
+
+        JLabel firstLabel = new JLabel(firstText);
+        JLabel secondLabel = new JLabel(secondText);
+
+        this.add(firstLabel);
+        this.add(Dimensions.V_SPACER);
+        this.add(firstPanel);
+
+        this.add(secondLabel);
+        this.add(Dimensions.V_SPACER);
+        this.add(secondPanel);
+
+        // Double click listener for first group
         MouseListener mouseListener = new MouseAdapter() {
             public void mouseClicked(MouseEvent mouseEvent) {
                 JList jlist = (JList) mouseEvent.getSource();
@@ -61,21 +48,8 @@ class ListPanel extends JPanel {
                 }
             }
         };
-        jlist.addMouseListener(mouseListener);
+        firstList.addMouseListener(mouseListener);
 
     }
-    
-    // Test per la schermata di chat
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> {
-            try { UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName()); }
-            catch (Exception e) { e.printStackTrace(); }
-        });
-        String[] chats = {"Ciao", "Prova", "Zaphodias", "daquinoaldo"};
-        //JPanel panel = new ListPanel(chats);
-        for(String chat : chats)
-            State.addFriend(chat);
-        JPanel panel = new ListPanel(State.friends(), true);
-        Utils.createWindow("Chat list", panel, Dimensions.LIST_PANE);
-    }
+
 }
---
+++ client/src/gui/panels/ListPanelFactory.java
@@ -0,0 +1,102 @@
+package gui.panels;
+
+import base.State.Friend;
+import constants.Dimensions;
+import gui.Utils;
+
+import javax.swing.*;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+class ListPanelFactory {
+
+    static JPanel newFriendPane(Collection<Friend> friends) {
+        List<String> online = new ArrayList<>();
+        List<String> offline = new ArrayList<>();
+        for (Friend friend : friends) {
+            if(friend.isOnline()) online.add(friend.getUsername());
+            else offline.add(friend.getUsername());
+        }
+        online.sort(String::compareToIgnoreCase);
+        online.sort(String::compareToIgnoreCase);
+
+        return new ListPanel(
+                "Online: double-click to open chat",
+                online.toArray(new String[online.size()]),
+                "Offline",
+                offline.toArray(new String[offline.size()])
+        );
+    }
+
+    /*static ListPanel newChatPane(Collection<Room> rooms) {
+        List<String> subscriptions = new ArrayList<>();
+        List<String> others = new ArrayList<>();
+        for (Room room : rooms) {
+            if(room.subscribed()) subscriptions.add(room.getName());
+            else others.add(room.getName());
+        }
+        subscriptions.sort(String::compareToIgnoreCase);
+        subscriptions.sort(String::compareToIgnoreCase);
+
+        return new ListPanel(
+                "My rooms: double-click to open chat",
+                subscriptions.toArray(new String[subscriptions.size()]),
+                "Other rooms",
+                others.toArray(new String[others.size()])
+        );
+    }*/
+
+
+    // Test per la schermata di chat
+    private static Collection<Friend> fakeFriends() {
+        Collection<Friend> friends = new ArrayList<>();
+        friends.add(new Friend("friend1", true));
+        friends.add(new Friend("friend2", false));
+        friends.add(new Friend("friend3", true));
+        friends.add(new Friend("friend4", true));
+        friends.add(new Friend("friend5", false));
+        friends.add(new Friend("friend6", false));
+        friends.add(new Friend("friend7", true));
+
+        friends.add(new Friend("friend1", true));
+        friends.add(new Friend("friend2", false));
+        friends.add(new Friend("friend3", true));
+        friends.add(new Friend("friend4", true));
+        friends.add(new Friend("friend5", false));
+        friends.add(new Friend("friend6", false));
+        friends.add(new Friend("friend7", true));
+        friends.add(new Friend("friend1", true));
+        friends.add(new Friend("friend2", false));
+        friends.add(new Friend("friend3", true));
+        friends.add(new Friend("friend4", true));
+        friends.add(new Friend("friend5", false));
+        friends.add(new Friend("friend6", false));
+        friends.add(new Friend("friend7", true));
+        friends.add(new Friend("friend1", true));
+        friends.add(new Friend("friend2", false));
+        friends.add(new Friend("friend3", true));
+        friends.add(new Friend("friend4", true));
+        friends.add(new Friend("friend5", false));
+        friends.add(new Friend("friend6", false));
+        friends.add(new Friend("friend7", true));
+        friends.add(new Friend("friend1", true));
+        friends.add(new Friend("friend2", false));
+        friends.add(new Friend("friend3", true));
+        friends.add(new Friend("friend4", true));
+        friends.add(new Friend("friend5", false));
+        friends.add(new Friend("friend6", false));
+        friends.add(new Friend("friend7", true));
+        return friends;
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            try { UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName()); }
+            catch (Exception e) { e.printStackTrace(); }
+        });
+        JPanel panel = ListPanelFactory.newFriendPane(fakeFriends());
+        Utils.createWindow("User list", panel, Dimensions.LIST_PANE);
+    }
+}
