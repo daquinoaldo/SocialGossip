8ae494b29b6ec31095b6413087eb4cc5ba0d585a
Aldo D'Aquino <daquino.aldo@gmail.com>
2018-01-11 15:33:41 +0100

    Test Multicast

---
+++ client/src/Connections/MainMulticastTest.java
@@ -0,0 +1,21 @@
+package Connections;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
+
+public class MainMulticastTest {
+
+    public static void main (String [ ] args) {
+        try {
+            MulticastSocket ms = new MulticastSocket();
+            InetAddress ia = InetAddress.getByName("239.8.185.226");
+            ms.joinGroup (ia);
+            System.out.println("Joinato");
+        } catch (IOException e) {
+            System.err.println("Errore");
+            e.printStackTrace();
+        }
+    }
+}
+
